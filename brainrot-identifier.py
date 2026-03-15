f5c43ab4e9d03fe32ce2511bcf2dfa038ad8d30f
Fanglin <157740592+fanglin0@users.noreply.github.com>
2026-03-15 12:04:32 -0400

    added brainrot-identifier

---
+++ brainrot-identifier.py
@@ -0,0 +1,18 @@
+import os
+import caer
+import canara
+import numpy as np
+import cv2 as cv
+import gc
+
+IMG_SIZE = (80,80)
+channels = 1
+
+char_path = r'/Users/fanglin/Downloads/brainrot_dataset'
+
+char_dict = {}
+for char in 
+
+ddd
+#make a web scraper for ianges ttagegd with different brainrots in 
+
